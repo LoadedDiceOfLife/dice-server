7a4b36a42b516b8c95671e4ebdd2fbbc89d7bd75
Milan Oberkirch <milan@oberkirch.org>
2015-07-06 10:26:20 +0000

    Added this repo to pull_them_all.py.

--- pull_them_all.py
+++ pull_them_all.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from __future__ import print_function
 
 #-Configuration--------------------------------------------------------------#
 
@@ -7,10 +8,12 @@ PORT = 8081
 FOLDERS = [
     '/var/www/play.zufallston.de/dice-engine/',
     '/var/www/play.zufallston.de/dice-static/',
-    '/var/www/play.zufallston.de/dice-parser/']
+    '/var/www/play.zufallston.de/dice-parser/',
+    '/home/hacker46/server-config/']
 
 #----------------------------------------------------------------------------#
 
+from sys import stderr
 from subprocess import Popen, PIPE
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
@@ -24,6 +27,14 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             output.append(
                 'folder: %s\nstderr: %s\nstdout: %s\n' % (
                     folder, err, out))
+        process = Popen(['sudo', 'nginx', '-t'], stdout=PIPE, stderr=PIPE)
+        out, err = process.communicate()
+        if not process.wait():
+            process = Popen(['sudo', 'systemctl', 'restart', 'nginx'])
+            output.append("restarting server")
+        else:
+            print(err, file=stderr)
+            output.append("nginx -t:\nstderr: %s\nstdout: %s" % (err, out))
         response = "<!DOCTYPE html><html><head><title>git</title></head>\
                     <body><pre>%s</pre></body></html>" % '\n'.join(output)
         self.send_response(200)
